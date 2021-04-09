e51e3306a8bedaa4ff74da5f338a5c08e30dbd26
tawan007 <tawan.mee@ku.th>
2021-04-09 18:33:23 +0700

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
-
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -17,7 +17,7 @@ def post_detail(request, pk):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
